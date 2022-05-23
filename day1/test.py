4173c7d11f7800ef72189bad39e444e7a2becc1a
kwogus1 <kwogus1@naver.com>
2022-05-23 17:13:50 +0900

    깃허브 최종 커밋

---
+++ day1/test.py
@@ -0,0 +1,31 @@
+print('Hello Python!')
+
+a = 'Hello, Python.' #a는 변수(값을 담는다. 값이 변함)
+print(a)
+
+a = 10
+print(a)
+
+a = False
+print(a)
+# 주석입니다. 주석은 소스 실행에 아무런 영향을 미치지 않습니다.
+# 변수명 지정: 영문자 _로 시작, 특수문자는 _만 사용, 숫자로 지정못함, 대소문자구분
+var1 = 17
+Var1 = 19
+바20 = 20
+print(var1)
+print(Var1)
+print(바20)
+#4var = 4
+#5_var = 5 
+var_5 = 5
+#var$7 = 7
+_var_8 = 8 
+var_8_ = 8.8
+#var!~56
+#var 10 = 10
+#RemainOfBankAccount # 은행계좌 잔고, 의미있는 변수명 지정
+
+print(var1 == Var1)
+
+# 자료 = 데이터 -> 정보, 지식
